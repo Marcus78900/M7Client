e845cceeb9688d7e542535a42e89962bde78a685
Marcus78900 <137833547+Marcus78900@users.noreply.github.com>
2026-08-18 07:31:56 -0300

    Add import for setContent in MainActivity

--- app/src/main/java/com/m7/client/MainActivity.kt
+++ app/src/main/java/com/m7/client/MainActivity.kt
@@ -7,6 +7,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.core.animateFloatAsState
